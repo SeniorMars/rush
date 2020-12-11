261cd754e5c69f0777e16cbb95d551cf3dd45c46
KarlWithK <jocelinc60@outlook.com>
2020-12-11 10:35:24 -0500

    Added parse_args function in input.c
    I believe we should make this recursively. We need to allocate space.

--- input.c
+++ input.c
@@ -6,41 +6,34 @@
 #define SIZE 6
 #define DEFAULT_BUFFER_SIZE 10
 
-char *args[SIZE];
-char *read_line()
-{
-    int size = DEFAULT_BUFFER_SIZE;
-    char *str;
-    str = (char *)calloc(size, sizeof(char));
-    char c;
-    c = getchar();
-    int i = 0;
-    while (c != '\n')
-    {
-        if (i >= size)
-        {
-            size = size * 2;
-            str = (char *)realloc(str, size);
-        }
-        str[i] = c;
-        c = getchar();
-        ++i;
+char *args[SIZE]; // We have to fix to have dynamic memory based on length of
+                  // read_line!!
+char *read_line() {
+  int size = DEFAULT_BUFFER_SIZE;
+  char *str;
+  str = (char *)calloc(size, sizeof(char));
+  char c;
+  c = getchar();
+  int i = 0;
+  while (c != '\n') {
+    if (i >= size) {
+      size = size * 2;
+      str = (char *)realloc(str, size);
     }
-    str[i] = '\0';
-    return str;
+    str[i] = c;
+    c = getchar();
+    ++i;
+  }
+  str[i] = '\0';
+  return str;
 }
 
 char **parse_args(char *line) {
-	for(int i = 0; i < SIZE; i++) {
-		args[i] = strsep(&line, " ");
-	}
-	return args;
-}
-
-
-int main() {
-	char line[] = "ls -a -l -h";
-  char ** args = parse_args(line);
-	execvp(args[0], args);
-  return 0;
+  for (int i = 0; line; i++) {
+    // I think we should perhaps try to separate by ";" first and
+    // call the this same function. Like recursively!
+    // We would have to allocate enough space for the array arcs
+    args[i] = strsep(&line, " ");
+  }
+  return args;
 }
