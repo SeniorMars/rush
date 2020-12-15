dc1934657f77eca99ac6cf2dbc39ccd541697063
Eisuke <kestrel1253@gmail.com>
2020-12-14 19:25:49 -0500

    Added exec function, need to figure out while loop for main method

--- input.c
+++ input.c
@@ -3,21 +3,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define SIZE 6
 #define DEFAULT_BUFFER_SIZE 10
 
 /* char *args[SIZE]; // We have to fix to have dynamic memory based on length of
  */
 // read_line!!
-char *read_line() {
+char *read_line()
+{
   int size = DEFAULT_BUFFER_SIZE;
   char *str;
   str = (char *)calloc(size, sizeof(char));
   char c;
   c = getchar();
   int i = 0;
-  while (c != '\n') {
-    if (i >= size) {
+  while (c != '\n')
+  {
+    if (i >= size)
+    {
       size = size * 2;
       str = (char *)realloc(str, size);
     }
@@ -30,9 +35,11 @@ char *read_line() {
 }
 
 char *args[DEFAULT_BUFFER_SIZE]; // arugments
-char **parse_args(char *line) {
-    // dynamic allocate buffer size
-  for (int i = 0; line; i++) {
+char **parse_args(char *line)
+{
+  // dynamic allocate buffer size
+  for (int i = 0; line; i++)
+  {
     // I think we should perhaps try to separate by ";" first and
     // call the this same function. Like recursively.
     // We would have to allocate enough space for the array arcs
@@ -42,31 +49,61 @@ char **parse_args(char *line) {
   return args;
 }
 
-void count(int *cmds, int *args, char *line) {
-  for (int i = 0; i < (int) strlen(line); i++) {
-    if (line[i] == ' ') {
+void exec(char ** args)
+{
+  int c, status;
+  printf("Initial msg to check\n");
+
+  c = fork();
+  if (!c)
+  {
+    execvp(args[0], args);
+  }
+  else
+  {
+    wait(&status);
+    printf("Command executed\n");
+  }
+}
+
+void count(int *cmds, int *args, char *line)
+{
+  for (int i = 0; i < (int)strlen(line); i++)
+  {
+    if (line[i] == ' ')
+    {
       (*args)++;
     }
-    if (line[i] == ';') {
+    if (line[i] == ';')
+    {
       (*cmds)++;
     }
   }
 }
 
-
-int main() {
+int main()
+{
   // all the below should be in a while loop. Unless when we parse line it's "quit"
-  char *line = read_line();
-  int commands = 1, arguments = 1; // start at once because there will always be one command ad one argument
-  int *cmdp = &commands, *argsp = &arguments;
-  count(cmdp, argsp, line); // gets number of arguments and commands
-  printf("%d\n", *argsp);
-  // parse amount of arguments;
-  /* for (int i = 0; i < strlen(line); i++) { */
-  /*   printf("%c\n", line[i]); */
-  /* } */
-  /* int len = strlen(line); */
-  char **args = parse_args(line);
-  execvp(args[0], args);
+  //while (1)
+  //{
+    char *line = read_line();
+    int commands = 1, arguments = 1; // start at once because there will always be one command ad one argument
+    int *cmdp = &commands, *argsp = &arguments;
+    count(cmdp, argsp, line); // gets number of arguments and commands
+    printf("%d\n", *argsp);
+    // parse amount of arguments;
+    /* for (int i = 0; i < strlen(line); i++) { */
+    /*   printf("%c\n", line[i]); */
+    /* } */
+    /* int len = strlen(line); */
+    char **args = parse_args(line);
+    /*
+    if (strcmp(args[0], "quit") == 0)
+    {
+      break;
+    }
+    */
+    exec(args);
+  //}
   return 0;
 }
