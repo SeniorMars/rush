902c41966495f087e96f39765b1442205a22d9a0
Amit Prasad <17amitprasad@gmail.com>
2020-12-19 22:18:07 -0500

    running cd with no arguments sends you to home directory

--- input.h
+++ input.h
@@ -4,6 +4,7 @@
 char *read_line();
 // parses from line
 char **parse_args(char *line, const char *to_split);
+
 void count(int *cmds, int *args, char *line);
 int exec(char **args);
 #endif
--- rush.c
+++ rush.c
@@ -12,18 +12,29 @@ char last_working_dir[PATH_MAX + 1];
 
 int main()
 {
+    char *home = getenv("HOME");
     getcwd(last_working_dir, sizeof(last_working_dir));
     while (1)
     {
+        //The user@machine, etc.
         prompt();
         char *line = read_line();
         if (strlen(line) > 0)
         {
+            //List of commands, separated by ;
             char **cmds = parse_args(line, ";");
             char **p = cmds;
             for (char *command = *p; command; command = *++p)
             {
                 char **args = parse_args(command, " ");
+                //Count number of args
+                int arglen = 0;
+                char **p2 = args;
+                for (char* c = *p2; c; c = *++p2)
+                {
+                    ++arglen;
+                }
+                //Builtin exit function
                 if (strcmp(args[0], "exit") == 0)
                 {
                     printf("exiting...\n");
@@ -32,19 +43,29 @@ int main()
                     free(cmds);
                     goto end;
                 }
+                //Builtin cd function
                 else if (strcmp(args[0], "cd") == 0)
                 {
-                    char *target_dir = args[1];
                     char temp[PATH_MAX + 1];
                     getcwd(temp, sizeof(temp));
                     int rc;
-                    if (strcmp(target_dir, "-") == 0)
+                    //If the command is run with only "cd"
+                    if (arglen < 2)
                     {
-                        rc = chdir(last_working_dir);
+                        rc = chdir(home);
                     }
                     else
                     {
-                        rc = chdir(target_dir);
+                        //Handle cd to last directory
+                        char *target_dir = args[1];
+                        if (strcmp(target_dir, "-") == 0)
+                        {
+                            rc = chdir(last_working_dir);
+                        }
+                        else
+                        {
+                            rc = chdir(target_dir);
+                        }
                     }
                     if (rc == -1)
                     {
